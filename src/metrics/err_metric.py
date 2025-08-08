49bdf261ac31ea753f4cb556eda6e759375a9f72
den.akifev <den.akifev@mail.ru>
2025-08-08 11:11:37 +0300

    fix error in metric

--- src/metrics/err_metric.py
+++ src/metrics/err_metric.py
@@ -30,9 +30,9 @@ def compute_det_curve(target_scores, nontarget_scores):
         np.arange(1, n_scores + 1) - tar_trial_sums
     )
 
-    frr = np.concatenate((np.atleast_1d(0), tar_trial_sums / target_scores.size(0)))
+    frr = np.concatenate((np.atleast_1d(0), tar_trial_sums / target_scores.size))
     far = np.concatenate(
-        (np.atleast_1d(1), nontarget_trial_sums / nontarget_scores.size(0))
+        (np.atleast_1d(1), nontarget_trial_sums / nontarget_scores.size)
     )
 
     return frr, far
